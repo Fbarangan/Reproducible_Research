7b40a17b0f96a829ab7424650a9e32f9a7ea770d
Fbarangan <franb86.2@gmail.com>
2016-05-09 18:59:44 -0400

    fix download by adding method = "curl"

--- Assignment2/Storm.R
+++ Assignment2/Storm.R
@@ -1,1 +1,9 @@
 # Course Assignment 2
+# data is from   https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2
+#
+# info https://d396qusza40orc.cloudfront.net/repdata%2Fpeer2_doc%2Fpd01016005curr.pdf
+#
+# FAQ https://d396qusza40orc.cloudfront.net/repdata%2Fpeer2_doc%2FNCDC%20Storm%20Events-FAQ%20Page.pdf
+#
+# Title
+#
