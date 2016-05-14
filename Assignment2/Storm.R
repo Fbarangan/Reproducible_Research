7b357db46f0819e1a6ee336620d2627129612527
Fbarangan <franb86.2@gmail.com>
2016-05-14 00:16:12 -0400

    fiter for only those with injuries and fatalities

--- Assignment2/Storm.R
+++ Assignment2/Storm.R
@@ -28,83 +28,109 @@ rawStormDataDF_ <- tbl_df(rawStormData)
 rawStormDataDF_$EVTYPE <- tolower(rawStormDataDF_$EVTYPE)
 # Select Variable
 
+
+# Select only those fatalities and Injury > 0
+
+StormDataDF_Fatalities_Injury <- rawStormDataDF_ %>%
+select(State = STATE, Event_Type = EVTYPE, Fatalities = FATALITIES, Injuries = INJURIES) %>%
+        filter( Fatalities > 0 & Injuries > 0)
+
 # Storm Surge
-rawStormDataDF_stormSurge <- rawStormDataDF_ %>%
-        select(State = STATE, Event_Type = EVTYPE, Fatalities = FATALITIES, Injuries = INJURIES) %>%
-        filter(Event_Type == "storm surge")
+rawStormDataDF_stormSurge <- StormDataDF_Fatalities_Injury %>%
+        select(State, Event_Type, Fatalities, Injuries) %>%
+        filter(Event_Type %in% c ("storm surge","coastal storm","dust storm","tropical storm"))
 
 # Flood
-rawStormDataDF_flood <- rawStormDataDF_ %>%
-        select(State = STATE, Event_Type = EVTYPE, Fatalities = FATALITIES, Injuries = INJURIES) %>%
-        filter(Event_Type == "flood")
+rawStormDataDF_flood <- StormDataDF_Fatalities_Injury %>%
+        select(State, Event_Type, Fatalities, Injuries ) %>%
+        filter(Event_Type %in% c("flooding","flood","flash flood","flood/flash flood"))
 
 # Tornado
-rawStormDataDF_tornado <- rawStormDataDF_ %>%
-        select(State = STATE, Event_Type = EVTYPE, Fatalities = FATALITIES, Injuries = INJURIES) %>%
-        filter(Event_Type == "tornado")
+rawStormDataDF_tornado <- StormDataDF_Fatalities_Injury %>%
+        select(State, Event_Type, Fatalities, Injuries) %>%
+        filter(Event_Type  %in% c("tornado","waterspout/tornado"))
 
 # Wintry
-rawStormDataDF_wintry <- rawStormDataDF_ %>%
-        select(State = STATE, Event_Type = EVTYPE, Fatalities = FATALITIES, Injuries = INJURIES) %>%
-        filter(Event_Type %in% c("snow","ice","wintry","freeze","blizzard","cold","winter"))
+rawStormDataDF_wintry <- StormDataDF_Fatalities_Injury %>%
+        select(State, Event_Type, Fatalities, Injuries) %>%
+        filter(Event_Type %in% c("ice storm","ice","black ice","winter storm","winter storm high winds","winter weather/mix","cold","thundersnow","heavy snow","blowing snow","snow","rain/snow","light snow","freezing rain","extreme windchill"))
 
 # Rain
-rawStormDataDF_Rain <- rawStormDataDF_ %>%
-        select(State = STATE, Event_Type = EVTYPE, Fatalities = FATALITIES, Injuries = INJURIES) %>%
-        filter(Event_Type %in% c("rain","shower"))
+rawStormDataDF_Rain <- StormDataDF_Fatalities_Injury %>%
+        select(State, Event_Type, Fatalities, Injuries) %>%
+        filter(Event_Type %in% c("rain","heavy rain","excessive rainfall"))
+
+# Lightning
+rawStormDataDF_lightning <- StormDataDF_Fatalities_Injury %>%
+        select(State , Event_Type , Fatalities , Injuries) %>%
+        filter(Event_Type %in% c("lightning"))
+
 
 # Wind
-rawStormDataDF_wind <- rawStormDataDF_ %>%
-        select(State = STATE, Event_Type = EVTYPE, Fatalities = FATALITIES, Injuries = INJURIES) %>%
-        filter(Event_Type %in% c("wind"))
+rawStormDataDF_wind <- StormDataDF_Fatalities_Injury %>%
+        select(State , Event_Type , Fatalities , Injuries ) %>%
+        filter(Event_Type %in% c("wind","tstm wind","high wind","high winds/snow","strong wind","thunderstorm winds"))
 
 # Hurricane
-rawStormDataDF_Hurricane <- rawStormDataDF_ %>%
-        select(State = STATE, Event_Type = EVTYPE, Fatalities = FATALITIES, Injuries = INJURIES) %>%
-        filter(Event_Type %in% c("hurricane","tropical","typhoon"))
+rawStormDataDF_Hurricane <- StormDataDF_Fatalities_Injury %>%
+        select(State , Event_Type , Fatalities , Injuries ) %>%
+        filter(Event_Type %in% c("hurricane","tropical","typhoon","hurricane/typhoon"))
 
 # Dry
-rawStormDataDF_Dry_weather <- rawStormDataDF_ %>%
-        select(State = STATE, Event_Type = EVTYPE, Fatalities = FATALITIES, Injuries = INJURIES) %>%
+rawStormDataDF_Dry_weather <- StormDataDF_Fatalities_Injury %>%
+        select(State , Event_Type , Fatalities , Injuries ) %>%
         filter(Event_Type %in% c("dry","drought"))
 
 # Heat
-rawStormDataDF_Heat <- rawStormDataDF_ %>%
-        select(State = STATE, Event_Type = EVTYPE, Fatalities = FATALITIES, Injuries = INJURIES) %>%
-        filter(Event_Type %in% c("heat","warm"))
+rawStormDataDF_Heat <- StormDataDF_Fatalities_Injury %>%
+        select(State , Event_Type , Fatalities , Injuries ) %>%
+        filter(Event_Type %in% c("heat","warm","excessive heat"))
 
 # Hail
-rawStormDataDF_Hail <- rawStormDataDF_ %>%
-        select(State = STATE, Event_Type = EVTYPE, Fatalities = FATALITIES, Injuries = INJURIES) %>%
-        filter(Event_Type %in% c("hail"))
+rawStormDataDF_Hail <- StormDataDF_Fatalities_Injury %>%
+        select(State , Event_Type , Fatalities , Injuries ) %>%
+        filter(Event_Type %in% c("hail","tstm wind/hail"))
 
 # Fire
-rawStormDataDF_Fire <- rawStormDataDF_ %>%
-        select(State = STATE, Event_Type = EVTYPE, Fatalities = FATALITIES, Injuries = INJURIES) %>%
-        filter(Event_Type %in% c("fire"))
+rawStormDataDF_Fire <- StormDataDF_Fatalities_Injury %>%
+        select(State, Event_Type, Fatalities, Injuries) %>%
+        filter(Event_Type %in% c("fire","wild fires","wild/forest fire","wildfire"))
 # Others
-rawStormDataDF_Others <- rawStormDataDF_ %>%
-        select(State = STATE, Event_Type = EVTYPE, Fatalities = FATALITIES, Injuries = INJURIES) %>%
-        filter(Event_Type != "storm surge",
-               Event_Type != "flood",
-               Event_Type != "tornado",
-               Event_Type != "snow",
-               Event_Type != "ice",
-               Event_Type != "wintry",
-               Event_Type != "freeze",
-               Event_Type != "blizzard",
-               Event_Type != "cold",
-               Event_Type != "winter",
-               Event_Type != "rain",
-               Event_Type != "shower",
-               Event_Type != "wind",
-               Event_Type != "hurricane",
-               Event_Type != "tropical",
-               Event_Type != "typhoon",
-               Event_Type != "dry",
-               Event_Type != "drought",
-               Event_Type != "heat",
-               Event_Type != "warm",
-               Event_Type != "hail",
-               Event_Type != "fire")
+rawStormDataDF_Others <- StormDataDF_Fatalities_Injury %>%
+        select(State , Event_Type , Fatalities , Injuries ) %>%
+        filter( Event_Type != "tornado",
+                Event_Type != "waterspout/tornado",
+                Event_Type !=  "storm surge",
+                Event_Type != "tropical storm",
+                Event_Type !=  "coastal storm",
+                Event_Type !=  "dust storm",
+                Event_Type != "flooding",
+                Event_Type != "flood",
+                Event_Type != "flash flood",
+                Event_Type != "flood/flash flood",
+                Event_Type != "ice storm",
+                Event_Type != "ice",
+                Event_Type != "black ice",
+                Event_Type != "winter storm",
+                Event_Type != "winter storm high winds",
+                Event_Type != "winter weather/mix",
+                Event_Type != "cold",
+                Event_Type != "thundersnow",
+                Event_Type != "heavy snow",
+                Event_Type != "blowing snow",
+                Event_Type != "snow",
+                Event_Type !="extreme windchill",
+                Event_Type != "rain/snow",
+                Event_Type != "light snow",
+                Event_Type != "freezing rain",
+                Event_Type != "rain",
+                Event_Type != "heavy rain",
+                Event_Type != "excessive rainfall",
+                Event_Type != "lightning",
+                Event_Type !="wind",
+                Event_Type !="tstm wind",
+                Event_Type !="high wind",
+                Event_Type !="high winds/snow",
+                Event_Type != "strong wind",
+                Event_Type != "thunderstorm winds")
 
